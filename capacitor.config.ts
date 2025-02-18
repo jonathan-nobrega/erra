b9b195f57e9a6efbbeb657c84129033c91c03cb2
Jonathan Moraes <eng.jonathan.moraes@gmail.com>
2025-02-18 11:59:05 -0500

    add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.erra.app',
+  appName: 'erra',
+  webDir: 'www'
+};
+
+export default config;
